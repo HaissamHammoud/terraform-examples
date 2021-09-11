66bed669cff723dcffda79ce62fc3c11e555d090
haissam.fawaz <haissam.fawaz@trocosimples.combr>
2021-09-10 22:18:36 -0300

    Reorganize files

---
+++ lambda/iam.tf
@@ -0,0 +1,40 @@
+/*
+  Create user role for lambda function
+*/
+data "aws_iam_policy_document" "lambda_assume_role_policy" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-lambdaRole-waf"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "lambda_role_policy_document" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:Get*",
+      "s3:List*"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "lambda_waf_policy" {
+  name = "lambda-waf-policy"
+  policy = data.aws_iam_policy_document.lambda_role_policy_document.json
+  role = aws_iam_role.lambda_role.id
+}
