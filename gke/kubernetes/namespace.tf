5734b96fad9ab1e07ff190f656e67e13b789c21f
Haissam Fawaz <51242705+HaissamHammoud@users.noreply.github.com>
2023-05-10 14:31:10 +0000

    feature(gke) comment default namespace from gke module

--- gke/kubernetes/namespace.tf
+++ gke/kubernetes/namespace.tf
@@ -2,16 +2,16 @@
 # here we can recieve a list of namespaces and deman it to make all tha namespaces wanter by changing de values on it dinamically 
 # eg default_namespaces = [ application, monitoring]
 
-data "template_file" "namespaces" {
-  count = var.namespaces
-  template = file("${path.module}/manifest/namespace.yaml.tpl")
-  vars = {
-    namespace = var.namespaces[count.index]
-  }
-}
-
-resource "kubernetes_manifest" "application-namespace" {
-    count = template_file.namespaces
-    provider=kubernetes
-    manifest = yamldecode(file(template_file[count.index]))
-}
+# data "template_file" "namespaces" {
+#   count = var.namespaces
+#   template = file("${path.module}/manifest/namespace.yaml.tpl")
+#   vars = {
+#     namespace = var.namespaces[count.index]
+#   }
+# }
+# 
+# resource "kubernetes_manifest" "application-namespace" {
+#     count = template_file.namespaces
+#     provider=kubernetes
+#     manifest = yamldecode(file(template_file[count.index]))
+# }
